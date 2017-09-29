1399f28076edd708654376d66e883f184b5944b1
Mariana Godoy <mariana@websdone.co.uk>
2017-09-29 23:09:48 +0100

    map ingredientes even if there is none

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -15,9 +16,22 @@ export class DataStorageService {
 
   getRecipes(){
     this.http.get('https://receipelist-fa406.firebaseio.com/recipes.json')
-      .subscribe(
+      .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              console.log(recipe);
+              recipe['ingredients'] = [];
+
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          console.log(recipes);
           this.recipeService.setRecipes(recipes);
         }
       );
